2124f8b13ade10ef8628edca59e4178d20e5f9b9
Kirbyrawr <devkirbyrawr@gmail.com>
2026-09-14 22:03:20 +0200

    Docs for deki-wifi from deki-editor f449cd3

--- navtreedata.js
+++ navtreedata.js
@@ -24,7 +24,12 @@
 */
 var NAVTREE =
 [
-  [ "deki-wifi", "index.html", ]
+  [ "deki-wifi", "index.html", [
+    [ "Changelog", "index.html#autotoc_md0", [
+      [ "0.15.0", "index.html#autotoc_md1", null ],
+      [ "Changed", "index.html#autotoc_md2", null ]
+    ] ]
+  ] ]
 ];
 
 var NAVTREEINDEX =
